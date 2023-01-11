b1688fa511d60315861053011f8d1a1a29effa84
eriket0107 <ol.erik0107@gmail.com>
2023-01-11 08:40:49 -0300

    user update function

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -22,11 +22,45 @@ class UserController{
       password: hashedPassword
     })
     
-    return res.json({name, email, password: hashedPassword})
+    return res.status(201).json("Usuário cadastrado com sucesso!")
   }
 
   async update(req,res){
+    const {email, name, password, old_password} = req.body
+    const user_id = req.user.id
+
+    const user = await knex('users').select('*').where({id: user_id}).first()
+
+    if(!user) throw new AppError("Usuário não encontrado")
+
+    const emailExists = await knex("users").where({email}).first()
     
+    if(emailExists && emailExists.id !== user_id) throw new AppError('Esse email já está em uso')
+ 
+    user.name = name ?? user.name
+    user.email = email ?? user.name
+
+    if(password && !old_password || !password && old_password) 
+      throw new AppError("Você precisa informas as duas senhas para definir uma nova.")
+
+    if(password && old_password){
+      const checkPassword = await compare(old_password, user.password)
+
+      if(!checkPassword) throw new AppError('Senha atual inválida')
+
+      user.password = await hash(password, 8)
+    }
+
+    await knex("users")
+    .select('*')
+    .update({
+      "email": email, 
+      "name": name, 
+      "password": user.password, 
+      "updated_at": knex.fn.now()
+    }).where({id: user_id})
+
+    return res.json({user, test: "Cadastro alterado com sucesso!"})
   }
 }
 
