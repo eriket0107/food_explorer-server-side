f144251902a7698218a99a2987e086e646291e34
eriket0107 <ol.erik0107@gmail.com>
2023-01-11 06:21:05 -0300

    setting up all migrations

--- src/db/knex/migrations/20230111085458_create_orders_table.js
+++ src/db/knex/migrations/20230111085458_create_orders_table.js
@@ -1,7 +1,7 @@
 exports.up = knex => knex.schema.createTable("orders", table=>{
   table.increments("id")
-  table.intenger("user_id").references("id").inTable("users")
-  table.intenger("dish_id").references("id").inTable("dishes")
+  table.integer("user_id").references("id").inTable("users")
+  table.integer("dish_id").references("id").inTable("dishes")
 
   table.string('status', 255).default('Pendente')
   table.timestamp('created_at').default(knex.fn.now())
