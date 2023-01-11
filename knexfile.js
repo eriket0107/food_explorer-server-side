526c4c43be9fdd93e157dc0232077efc1b889b96
eriket0107 <ol.erik0107@gmail.com>
2023-01-11 05:56:16 -0300

    setting up migrations

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, "src", "database", "database.db")
+      filename: path.resolve(__dirname, "src", "db", "database.db")
       // partindo desta pasta, acesse a pasta src, dentro dela a pasta database, dentro dela o arquivo database.db
     },
     pool: { // executado no momento de estabelecer conexão com o banco de dados
@@ -13,7 +13,7 @@ module.exports = {
       // PRAGMA foreign_keys = ON para habilitar a funcionalidade de, quando deletar, por exemplo, uma nota, deletar em cascata as tags
     },
     migrations: { 
-      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+      directory: path.resolve(__dirname, "src", "db", "knex", "migrations")
     },
     useNullAsDefault: true
   }
---
+++ src/db/knex/migrations/20230111083704_create_users_table.js
@@ -0,0 +1,14 @@
+exports.up = knex => knex.schema.createTable("users", table=>{
+  table.increments("id")
+  table.boolean("is_admin").default(false)
+  table.text('name')
+  table.text('email')
+  table.text('password')
+
+  table.timestamp('created_at').default(knex.fn.now())
+  table.timestamp('updated_at').default(knex.fn.now())
+
+  table.varchar('avatar')
+});
+
+exports.down = knex => knex.schema.dropTable("users")
