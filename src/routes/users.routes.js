d610246fa76c91d74ed98a546cf666f341004543
eriket0107 <ol.erik0107@gmail.com>
2023-01-11 08:40:17 -0300

    session authorization created

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,13 @@
 const { Router } = require('express')
 const UserController = require('../controllers/UserController')
+const ensureAuth = require('../middlewares/ensureAuth')
 
 const userController = new UserController()
 
 const userRoutes = Router()
 
+
 userRoutes.post('/', userController.create)
-userRoutes.put('/', userController.update)
+userRoutes.put('/', ensureAuth ,userController.update)
 
 module.exports = userRoutes
