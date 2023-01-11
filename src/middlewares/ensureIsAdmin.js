1e2ffb9c54066d3cfb150b4ad6a4ef885bb6295c
eriket0107 <ol.erik0107@gmail.com>
2023-01-11 11:03:09 -0300

    create mmiddleware ensureIsAdmin

---
+++ src/middlewares/ensureIsAdmin.js
@@ -0,0 +1,14 @@
+const knex = require("../db/knex")
+const AppError = require("../utils/appError")
+
+async function ensureIsAdmin(req, res, next){
+  const user_id = req.user.id
+
+  const user = await knex("users").where({id: user_id}).first()
+
+  if(!user.isAdmin) throw new AppError("Access Denied", 401)
+
+  next()
+}
+
+module.exports = ensureIsAdmin
