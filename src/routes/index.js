efe826f09953d6da5afa4c17ee57d4c123294ea8
eriket0107 <ol.erik0107@gmail.com>
2023-01-11 08:40:07 -0300

    session authorization created

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ const { Router } = require('express')
 const routes = Router()
 
 const userRoutes = require('./users.routes')
+const sessionRoutes = require('./session.routes')
 
 routes.use("/users", userRoutes)
+routes.use("/sessions", sessionRoutes)
 
 module.exports = routes
