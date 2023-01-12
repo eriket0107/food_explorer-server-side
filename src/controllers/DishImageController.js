fad072421a97d4df0c66675c929fa42149669b51
eriket0107 <ol.erik0107@gmail.com>
2023-01-12 15:42:56 -0300

    adding dishesImageController

---
+++ src/controllers/DishImageController.js
@@ -0,0 +1,27 @@
+const knex = require('../db/knex')
+const AppError = require('../utils/appError')
+const DiskStorage = require("../providers/DiskStorage")
+
+class DishImageController{
+  async update(req, res){
+    const { id } = req.params
+    const foodFileName = req.file.filename
+
+    const diskStorage = new DiskStorage()
+
+    const dish = await knex("dishes").where({id}).first()
+
+
+    if(dish.foodImg){
+      await diskStorage.deleteFile(dish.foodImg)
+    }
+    const file = await diskStorage.saveFile(foodFileName)
+    dish.foodImg = file
+
+    await knex('dishes').update(dish).where({id})
+
+    return res.json(file)
+  }
+}
+
+module.exports = DishImageController
