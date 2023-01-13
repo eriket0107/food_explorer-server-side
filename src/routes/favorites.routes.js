8073a89953c4cecc76cd3f54dc3063b9b92ddf7a
eriket0107 <ol.erik0107@gmail.com>
2023-01-13 14:10:18 -0300

    creating favorite routes

---
+++ src/routes/favorites.routes.js
@@ -0,0 +1,16 @@
+const { Router } = require('express')
+const ensureAuth = require('../middlewares/ensureAuth')
+
+const FavoritesController = require('../controllers/FavoritesController')
+
+const favoritesController = new FavoritesController
+
+const favoritesRoutes = Router()
+
+
+
+favoritesRoutes.get('/', ensureAuth, favoritesController.index)
+favoritesRoutes.post('/:id', ensureAuth, favoritesController.favorite)
+favoritesRoutes.put('/:id', ensureAuth, favoritesController.unfavorite)
+
+module.exports = favoritesRoutes
