dcac156940f8c90bfebcf8bf15fa0db8e6f4b1d7
eriket0107 <ol.erik0107@gmail.com>
2023-01-13 14:11:29 -0300

    adding favorite routes to index routes file

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ const { Router } = require('express')
 
 const routes = Router()
 
-const dishRoutes = require('./dish.routes')
 const userRoutes = require('./users.routes')
+const dishRoutes = require('./dish.routes')
 const sessionRoutes = require('./session.routes')
+const favoriteRoutes = require('./favorites.routes')
 
 routes.use("/users", userRoutes)
 routes.use("/dishes", dishRoutes)
 routes.use("/sessions", sessionRoutes)
+routes.use("/favorites", favoriteRoutes)
 
 module.exports = routes
