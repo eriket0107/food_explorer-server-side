eded9380f2d35b44befafdb9c96525b731fe96c4
eriket0107 <ol.erik0107@gmail.com>
2023-01-17 09:00:01 -0300

    creating orders controller

---
+++ src/routes/orders.routes.js
@@ -0,0 +1,19 @@
+const { Router } = require('express')
+const OrdersController = require('../controllers/OrdersController')
+const ensureIsAdmin = require('../middlewares/ensureIsAdmin')
+const ensureAuth = require('../middlewares/ensureAuth')
+
+const ordersController = new OrdersController()
+
+
+
+const ordersRoutes = Router()
+
+ordersRoutes.use(ensureAuth);
+
+
+ordersRoutes.get("/", ordersController.index);
+ordersRoutes.post("/", ensureAuth, ordersController.create)
+ordersRoutes.put("/", ensureIsAdmin , ordersController.update);
+
+module.exports = ordersRoutes
