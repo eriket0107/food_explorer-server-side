08727a1c83d1b5f6e4cbe9478ca7c961bd48beda
eriket0107 <ol.erik0107@gmail.com>
2023-01-17 08:59:43 -0300

    creating orders controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,15 @@ const { Router } = require('express')
 
 const routes = Router()
 
-const userRoutes = require('./users.routes')
 const dishRoutes = require('./dish.routes')
+const userRoutes = require('./users.routes')
+const ordersRoutes = require('./orders.routes')
 const sessionRoutes = require('./session.routes')
 const favoriteRoutes = require('./favorites.routes')
 
 routes.use("/users", userRoutes)
 routes.use("/dishes", dishRoutes)
+routes.use("/orders", ordersRoutes)
 routes.use("/sessions", sessionRoutes)
 routes.use("/favorites", favoriteRoutes)
 
