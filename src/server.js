d6544dbf53fa416995e5c60f8144e1405588043a
eriket0107 <ol.erik0107@gmail.com>
2023-02-24 09:40:47 -0300

    setting dotenv

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv/config');
 require('express-async-errors');
 
 const cors = require('cors');
@@ -13,28 +14,26 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 app.use('/files', express.static(uploadConfig.UPLOAD_FOLDER));
 
 app.use(routes);
 
+app.use((error, req, res, next) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
 
-app.use((error, req, res, next)=>{
-	if(error instanceof AppError){
-		return res.status(error.statusCode).json({
-			status: 'error',
-			message: error.message
-		});
-	}
-  
-	console.log(error);
-  
-	return res.status(500).json({
-		status: 'error',
-		message: 'Internal server'
-	});
+  console.log(error);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server',
+  });
 });
 
-const PORT = 3333;
+const PORT = process.env.SERVER_PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
