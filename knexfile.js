91e9a8d0cfc9b97424357def7f143b5d3d37ba6d
Erik <ol.erik0107@gmail.com>
2024-01-24 19:03:56 -0300

    chore: adding lint

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,21 @@
-const path = require("path");
+const path = require('path')
 
 module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, "src", "db", "database.db")
+      filename: path.resolve(__dirname, 'src', 'db', 'database.db'),
       // partindo desta pasta, acesse a pasta src, dentro dela a pasta database, dentro dela o arquivo database.db
     },
-    pool: { // executado no momento de estabelecer conexão com o banco de dados
-      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
+    pool: {
+      // executado no momento de estabelecer conexão com o banco de dados
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
       // após criar, recuperar a conexão e a função de callback
       // PRAGMA foreign_keys = ON para habilitar a funcionalidade de, quando deletar, por exemplo, uma nota, deletar em cascata as tags
     },
-    migrations: { 
-      directory: path.resolve(__dirname, "src", "db", "knex", "migrations")
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'db', 'knex', 'migrations'),
     },
-    useNullAsDefault: true
-  }
-};
+    useNullAsDefault: true,
+  },
+}
